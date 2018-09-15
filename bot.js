ea0719c9c84c7912aae6f8ad3791992a13f6cb51
PandaDNZ <43297045+PandaDNZ@users.noreply.github.com>
2018-09-15 08:54:36 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,6 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setGame('Guardian of NIGHT GAMERS server!')
 });
 
 client.on('message', msg => {
@@ -11,6 +10,15 @@ client.on('message', msg => {
     msg.reply('Panda e seful!');
   }
 });
+
+var game = {name: "with code"}; // sets game as "Playing with code"
+var streamingGame = {type: 1, name: "something", url: ""}; // "Streaming"
+// note: streaming status requires a valid twitch url:
+//       ex. "http://twitch.tv/channel"
+client.User.setGame(game); // playing
+client.User.setGame("with code"); // playing
+client.User.setGame(streamingGame); // streaming
+client.User.setGame(null); // not playing
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
