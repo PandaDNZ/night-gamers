c669d2ed17001af973960d8b12df1948d2d4b814
PandaDNZ <43297045+PandaDNZ@users.noreply.github.com>
2018-09-15 08:57:50 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
-    console.log('I am ready!');
+client.on("ready", () => {
+    client.user.setGame("with my code");
+});
+
+// NOTE: INTRODUCED IN VERSION 11.3 AND REPLACES setGame
+client.on("ready", () => {
+    client.user.setActivity({game: {name: "with my code", type: 0}});
 });
 
 client.on('message', msg => {
@@ -10,15 +15,6 @@ client.on('message', msg => {
     msg.reply('Panda e seful!');
   }
 });
-
-var game = {name: "with code"}; // sets game as "Playing with code"
-var streamingGame = {type: 1, name: "something", url: ""}; // "Streaming"
-// note: streaming status requires a valid twitch url:
-//       ex. "http://twitch.tv/channel"
-client.User.setGame(game); // playing
-client.User.setGame("with code"); // playing
-client.User.setGame(streamingGame); // streaming
-client.User.setGame(null); // not playing
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
