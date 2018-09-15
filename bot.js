def93fbc44d234ff86f0c3e88fae0b9363357abf
PandaDNZ <43297045+PandaDNZ@users.noreply.github.com>
2018-09-15 08:40:14 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setActivity({game: {name: "with my code", type: 0}});
+    client.user.setActivity('YouTube', { type: 'WATCHING' });
 });
 
 client.on('message', msg => {
