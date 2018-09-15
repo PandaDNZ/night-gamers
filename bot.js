bf241b951ca1c96056d8a76f3b9c064dba627c8e
PandaDNZ <43297045+PandaDNZ@users.noreply.github.com>
2018-09-15 07:41:03 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', () => {
+client.on('!ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
-    if (message.content === 'ping') {
+    if (message.content === '!ping') {
     	message.reply('pong');
   	}
 });
