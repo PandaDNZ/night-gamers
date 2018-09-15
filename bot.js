5bbaa8271bb5e9b60ffa0c8ea03b161c18c32999
PandaDNZ <43297045+PandaDNZ@users.noreply.github.com>
2018-09-15 08:02:29 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('!ready', () => {
+    bot.user.setStatus("dnd")
+    bot.user.setGame("PandaRO Seful meu!")
     console.log('I am ready!');
 });
 
