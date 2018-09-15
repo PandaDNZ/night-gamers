d68821d2dbc5f3071a6e2e1ce4c5c6dde3b48e22
PandaDNZ <43297045+PandaDNZ@users.noreply.github.com>
2018-09-15 08:11:56 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('!ready', () => {
+client.on('ready', () => {
     bot.user.setStatus("Online")
     bot.user.setGame("PandaRO Seful meu!")
     console.log('I am ready!');
 });
 
-client.on('message', message => {
-    if (message.content === '!ping') {
-    	message.reply('pong');
-  	}
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
 });
 
 // THIS  MUST  BE  THIS  WAY
