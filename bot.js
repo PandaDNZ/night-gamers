1b83d5e728f47cab097488f19312d69c1c9553e5
PandaDNZ <43297045+PandaDNZ@users.noreply.github.com>
2018-09-15 08:38:34 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-    client.user.setPresence({ activity: { name: 'with discord.js' }, status: 'idle' })
+    client.user.setActivity({game: {name: "with my code", type: 0}});
 });
 
 client.on('message', msg => {
