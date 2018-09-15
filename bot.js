146f80bbb7691d59475a0dc5cd1ac2d914384302
PandaDNZ <43297045+PandaDNZ@users.noreply.github.com>
2018-09-15 09:01:10 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,11 +5,6 @@ client.on("ready", () => {
     client.user.setGame("with my code");
 });
 
-// NOTE: INTRODUCED IN VERSION 11.3 AND REPLACES setGame
-client.on("ready", () => {
-    client.user.setActivity({game: {name: "with my code", type: 0}});
-});
-
 client.on('message', msg => {
   if (msg.content === 'Cine e seful aici?') {
     msg.reply('Panda e seful!');
